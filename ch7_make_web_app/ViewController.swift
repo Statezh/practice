519f9aa8c91c61308f3a3b7e0b2db7f80ae29c10
Statezh <statehyunsuh@gmail.com>
2022-11-01 23:28:52 +0900

    ch7_웹앱_만들기

---
+++ ch7_make_web_app/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  ch7_make_web_app
+//
+//  Created by 서주현 on 2022/11/01.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+
+    @IBOutlet var myWebView: WKWebView!
+    @IBOutlet var myActivityIndicator: UIActivityIndicatorView!
+    
+    func loadWebpage(_ url: String){
+        let myUrl = URL(string: url)
+        let myRequest = URLRequest(url: myUrl!)
+        myWebView.load(myRequest)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let filePath = Bundle.main.path(forResource: "htmlView", ofType: "html")
+        let myUrl = URL(fileURLWithPath: filePath!)
+        let myRequest = URLRequest(url: myUrl)
+        myWebView.load(myRequest)
+    }
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        myActivityIndicator.startAnimating()
+        myActivityIndicator.isHidden = false
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        myActivityIndicator.stopAnimating()
+        myActivityIndicator.isHidden = true
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        myActivityIndicator.stopAnimating()
+        myActivityIndicator.isHidden = true
+    }
+}
+
